2e58c8d5ccb828fbdf4a8792a993550f62a9f031
정현우 <jhw8449@gmail.com>
2024-11-15 17:04:34 +0900

    fix: 과제 8 번, QueryDSL 활용

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -1,23 +1,16 @@
 package org.example.expert.domain.todo.repository;
 
 import java.time.LocalDateTime;
-import java.util.Optional;
 
 import org.example.expert.domain.todo.entity.Todo;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-public interface TodoRepository extends JpaRepository<Todo, Long> {
+public interface TodoRepository extends JpaRepository<Todo, Long>, TodoRepositoryCustom {
 
-    @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u WHERE t.weather = :weather and t.modifiedAt = :modifidAt ORDER BY t.modifiedAt DESC")
+    @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u WHERE t.weather = :weather AND t.modifiedAt = :modifiedAt ORDER BY t.modifiedAt DESC")
     Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable, String weather, LocalDateTime modifiedAt);
 
-    @Query("SELECT t FROM Todo t " +
-            "LEFT JOIN t.user " +
-            "WHERE t.id = :todoId"
-        )
-    Optional<Todo> findByIdWithUser(@Param("todoId") Long todoId);
 }
